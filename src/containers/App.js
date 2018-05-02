8e932cb8a27c9f7a432ae1c5a28d696ac0805603
sierra <sierra@sierras-iMac.local>
2018-05-02 17:40:35 +0530

    files added

---
+++ src/containers/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import classes from'./App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit'
+ 
+class App extends Component {
+  state = {
+    persons: [
+      { id: 'frerf43', name: 'Max', age: 43},
+      { id: 'fdfafd3', name: 'Mac', age: 34},
+      { id: 'jjgy345', name: 'Stepf', age: 34}
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons });
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  render() {
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = <Persons persons={this.state.persons} clicked={this.deletePersonHandler} changed={this.nameChangedHandler} />;
+    }
+    
+
+    return (
+      <div className={classes.App}>
+        <Cockpit showPersons={this.state.showPersons} persons={this.state.persons} clicked={this.togglePersonsHandler} />
+        {persons}        
+      </div>
+    );
+  }
+}
+
+export default App;
